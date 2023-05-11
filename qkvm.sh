6770325dbaff2e1cc24e6fcbdbde846bd8f9f69e
haxerz.in <100017604+haxerzin@users.noreply.github.com>
2023-05-12 00:40:44 +0530

    Update qkvm.sh
    Add dependencies for GPU Passthrough

--- qkvm.sh
+++ qkvm.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-sudo apt update && sudo apt install -y qemu-kvm bridge-utils virt-manager qemu virt-viewer spice-vdagent && \
+sudo apt update && sudo apt install qemu-kvm bridge-utils virt-manager qemu virt-viewer spice-vdagent libhugetlbfs-bin libvirt-clients qemu-utils virt-manager ovmf && \
 sudo sed -i "s/#user = \"root\"/user = \"$(id -un)\"/g" /etc/libvirt/qemu.conf && \
 sudo sed -i "s/#group = \"root\"/group = \"$(id -gn)\"/g" /etc/libvirt/qemu.conf && \
 sudo usermod -a -G kvm $(id -un) && \
